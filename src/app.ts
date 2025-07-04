f750930867b76137977cdb73b4c5068b32625e69
Babul Akter <babulakterfsd@gmail.com>
2025-07-04 18:52:11 +0600

    live frontend url added to solve cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://hypestat-scrapper-frontend.vercel.app',
+    ],
     credentials: true,
   }),
 );
